e3fc5e1c0e26a9123d7b0153acaa6b124340554b
kohhorov09 <your-email@example.com>
2026-01-05 19:09:34 +0500

    edit vercel and package.json

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -4,13 +4,13 @@ const productsSlice = createSlice({
   initialState: [],
   name: "products",
   reducers: {
-   addToCard: (state, { payload }) => {
-  return !state.find((product) => product.id === payload.id)
-    ? [...state, { ...payload, amount: 1 }] // ... (nuqtalar) qo'shildi
-    : state.map((p) =>
-        p.id === payload.id ? { ...p, amount: p.amount + 1 } : p
-      );
-},
+    addToCard: (state, { payload }) => {
+      return !state.find((product) => product.id === payload.id)
+        ? [...state, { ...payload, amount: 1 }]
+        : state.map((p) =>
+            p.id === payload.id ? { ...p, amount: p.amount + 1 } : p
+          );
+    },
   },
 });
 export const { addToCard } = productsSlice.actions;
